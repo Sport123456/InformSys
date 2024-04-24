fa5c0c66cb5dd57e78369a4d62c5f4db096d0d01
Sport123456 <odolgopolov@mail.ru>
2024-04-25 01:04:28 +0500

    hhhhhhhhhhhh

--- my_app.py
+++ my_app.py
@@ -1,21 +1,51 @@
 # напиши здесь код основного приложения и первого экрана
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QVBoxLayout
-app=QApplication([])
-main_win=QWidget()
-main_win.show()
+from PyQt5.QtWidgets import (
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+from instr import *
+from second_win import *
+     
+class MainWin(QWidget):
+    def __init__(self):
+       ##''' окно, в котором располагается приветствие '''
+        super().__init__()
 
-text1=('Добро пожаловать в программу по определению состояния здоровья')
-sen1=QLabel(text1)
-text2=('Данное приложение позволит вам с помощбю теста Руфье провести первичную диагностику вашего здоровья\n'
-'Проба Руф1ье представляет собой')
-sen2=QLabel(text2)
-button=QPushButton('Начать')
+       # создаём и настраиваем графические элементы:
+        self.initUI()
 
-LayoutV=QVBoxLayout()
-LayoutV.addWidget(sen1,alignment=Qt.AlignCenter)
-LayoutV.addWidget(sen2,alignment=Qt.AlignCenter)
-LayoutV.addWidget(button,alignment=Qt.AlignCenter)
+       #устанавливает связи между элементами
+        self.connects()
 
-main_win.setLayout(LayoutV)
+       #устанавливает, как будет выглядеть окно (надпись, размер, место)
+        self.set_appear()
+
+       # старт:
+        self.show()
+
+    def initUI(self):
+      ## ''' создаёт графические элементы '''
+        self.btn_next = QPushButton(txt_next, self)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)         
+        self.setLayout(self.layout_line)
+  
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+   ##''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+app = QApplication([])
+mw = MainWin()
 app.exec_()
--- second_win.py
+++ second_win.py
@@ -1,37 +1,109 @@
 # напиши здесь код для второго экрана приложения
 from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication,QWidget,QPushButton,QLabel,QVBoxLayout,QVBoxLayout,QLineEdit,QHBoxLayout
-app=QApplication([])
-main_win=QWidget()
-main_win.show()
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
 
-text1=('Введите Ф.И.О')
-label1=QLabel(text1)
-ed1=QLineEdit()
-text2=('Полных лет')
-label2=QLabel(text2)
-ed2=QLineEdit()
-text3=('Лягте на спину и замерьте пульс за 15 секунд')
-label3=QLabel(text3)
-button1=QPushButton('Начать первый тест')
-text4=('Секундомер')
-label4=QLabel(text4)
 
-LayoutV1=QVBoxLayout()
-LayoutV2=QVBoxLayout()
-LayoutH1=QHBoxLayout()
+from instr import *
+from final_win import *
 
-LayoutV1.addWidget(label1,alignment=Qt.AlignLeft)
-LayoutV1.addWidget(ed1,alignment=Qt.AlignLeft)
-LayoutV1.addWidget(label2,alignment=Qt.AlignLeft)
-LayoutV1.addWidget(ed2,alignment=Qt.AlignLeft)
-LayoutV1.addWidget(label3,alignment=Qt.AlignLeft)
-LayoutV1.addWidget(button1,alignment=Qt.AlignLeft)
 
-##main_win.setLayout(LayoutV1)
-LayoutV2.addWidget(label4,alignment=Qt.AlignCenter)
+class TestWin(QWidget):
+    def __init__(self):
+       ##''' окно, в котором проводится опрос '''
+        super().__init__()
 
-LayoutH1.addLayout(LayoutV1)
-LayoutH1.addLayout(LayoutV2)
-main_win.setLayout(LayoutH1)
-app.exec_()
+
+       # создаём и настраиваем графические элементы:
+        self.initUI()
+
+
+       #устанавливает связи между элементами
+        self.connects()
+
+
+       #устанавливает, как будет выглядеть окно (надпись, размер, место)
+        self.set_appear()
+      
+       # старт:
+        self.show()
+  
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
+
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+
+
+   #''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
+
+
+    def initUI(self):
+       #''' создаёт графические элементы '''
+       #self.questionnary = AllQuestions()
+        self.btn_next = QPushButton(txt_sendresults, self)
+        self.btn_test1 = QPushButton(txt_starttest1, self)
+        self.btn_test2 = QPushButton(txt_starttest2, self)
+        self.btn_test3 = QPushButton(txt_starttest3, self)
+
+
+
+
+        self.text_name = QLabel(txt_name)
+        self.text_age = QLabel(txt_age)
+        self.text_test1 = QLabel(txt_test1)
+        self.text_test2 = QLabel(txt_test2)
+        self.text_test3 = QLabel(txt_test3)
+        self.text_timer = QLabel(txt_timer)
+
+        self.line_name = QLineEdit(txt_hintname)
+        self.line_age = QLineEdit(txt_hintage)
+
+
+        self.line_test1 = QLineEdit(txt_hinttest1)
+
+        self.line_test2 = QLineEdit(txt_hinttest2)
+
+        self.line_test3 = QLineEdit(txt_hinttest3)
+
+        self.l_line = QVBoxLayout()
+        self.r_line = QVBoxLayout()
+        self.h_line = QHBoxLayout()
+        self.r_line.addWidget(self.text_timer, alignment = Qt.AlignCenter)
+        self.l_line.addWidget(self.text_name, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_name, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.text_age, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_age, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test1, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.text_test3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_test3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test2, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.line_test3, alignment = Qt.AlignLeft)
+        self.l_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)
+        self.h_line.addLayout(self.l_line) 
+        self.h_line.addLayout(self.r_line)       
+        self.setLayout(self.h_line)
+  
+    def next_click(self):
+        self.hide()
+        self.fw = FinalWin()
+    def connects(self):
+        self.btn_next.clicked.connect(self.next_click)
+     #''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
